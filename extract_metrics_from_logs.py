588d0dbb9f803904266540cc7790e5dc3e96dcb7
sjagtap3 <sjagtap3@illinois.edu>
2024-11-10 23:39:18 -0600

    Log data to csv and script

---
+++ extract_metrics_from_logs.py
@@ -0,0 +1,45 @@
+import csv
+
+def parse_log_file(log_file):
+    attempts = []
+    attempt_data = {
+        "classification_type": 0,
+        "type_2": 0,
+        "total_attempts": 0
+    }
+
+    with open(log_file, 'r') as f:
+        for line in f:
+            if "************** New attempt started **************" in line:
+                if attempt_data["total_attempts"] > 0:
+                    attempts.append(attempt_data)
+                attempt_data = {
+                    "classification_type": 0,
+                    "type_2": 0,
+                    "total_attempts": 0
+                }
+            elif "Classification type captcha encountered" in line:
+                attempt_data["classification_type"] += 1
+            elif "Type 2 captcha encountered" in line:
+                attempt_data["type_2"] += 1
+            elif "Captcha is now solved, total attempts:" in line:
+                attempt_data["total_attempts"] = int(line.split(":")[1].strip())
+
+        # Append the last attempt
+        if attempt_data["total_attempts"] > 0:
+            attempts.append(attempt_data)
+
+    return attempts
+
+def save_to_csv(data, csv_file):
+    fieldnames = ["classification_type", "type_2", "total_attempts"]
+    with open(csv_file, 'w', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(data)
+
+log_file = 'captcha_log_file_v11.log'
+csv_file = 'captcha_data_v11_novpn.csv'
+
+data = parse_log_file(log_file)
+save_to_csv(data, csv_file)
